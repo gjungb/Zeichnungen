5e75aa7b7d5447bb5ebaeabcbb27e4e11158fc7a
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2020-04-28 11:53:23 +0200

    Node.js aktualisiert

--- Node.js
+++ Node.js
@@ -1,14 +1,14 @@
-<mxfile host="app.diagrams.net" modified="2020-04-28T09:47:29.251Z" agent="5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36" etag="RXDfYUcpK6KdU4epaxkn" version="13.0.2" type="github">
-  <diagram id="pGE_Ec8MFNf1zPf1OiBV" name="Seite-1">
+<mxfile host="app.diagrams.net" modified="2020-04-28T09:53:21.039Z" agent="5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36" etag="nMIL7qhtc0Q2tEjvDpO0" version="13.0.2" type="github">
+  <diagram id="pGE_Ec8MFNf1zPf1OiBV" name="Architektur">
     <mxGraphModel dx="1426" dy="803" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="1600" pageHeight="900" background="#CCFF99" math="0" shadow="0">
       <root>
         <mxCell id="0" />
         <mxCell id="1" parent="0" />
-        <mxCell id="KCzgvEdqQcpw7nO5eZ1D-1" value="&lt;h1&gt;Node.js&lt;/h1&gt;" style="rounded=1;whiteSpace=wrap;html=1;glass=0;labelPosition=center;verticalLabelPosition=top;align=center;verticalAlign=bottom;" vertex="1" parent="1">
-          <mxGeometry x="320" y="160" width="800" height="440" as="geometry" />
+        <mxCell id="KCzgvEdqQcpw7nO5eZ1D-1" value="&lt;pre&gt;&lt;font style=&quot;font-size: 32px&quot;&gt;Node.js&lt;/font&gt;&lt;/pre&gt;" style="rounded=1;whiteSpace=wrap;html=1;glass=0;labelPosition=left;verticalLabelPosition=middle;align=right;verticalAlign=middle;" vertex="1" parent="1">
+          <mxGeometry x="320" y="360" width="800" height="240" as="geometry" />
         </mxCell>
-        <mxCell id="KCzgvEdqQcpw7nO5eZ1D-4" value="OS" style="rounded=1;whiteSpace=wrap;html=1;glass=0;fontSize=32;" vertex="1" parent="1">
-          <mxGeometry x="320" y="640" width="800" height="60" as="geometry" />
+        <mxCell id="KCzgvEdqQcpw7nO5eZ1D-4" value="&lt;pre&gt;OS&lt;/pre&gt;" style="rounded=1;whiteSpace=wrap;html=1;glass=0;fontSize=32;labelPosition=left;verticalLabelPosition=middle;align=right;verticalAlign=middle;" vertex="1" parent="1">
+          <mxGeometry x="320" y="679" width="800" height="60" as="geometry" />
         </mxCell>
       </root>
     </mxGraphModel>
